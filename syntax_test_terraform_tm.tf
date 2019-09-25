2ccecb5bee3dd7e6ccd210671afd2af2e841f986
Pat Gaffney <pat@hypepat.com>
2019-09-24 19:55:25 -0500

    tmLanguage: rework comments and language constants

---
+++ syntax_test_terraform_tm.tf
@@ -0,0 +1,112 @@
+# SYNTAX TEST "Terraform.tmLanguage"
+
+/////////////////////////////////////////////////////////////////////
+// INLINE COMMENTS
+/////////////////////////////////////////////////////////////////////
+
+/////
+// Start of line inline comments with `#`.
+/////
+
+# inline comment
+# ^ source.terraform comment.line.terraform
+
+/////
+// Start of line inline comments with `//`.
+/////
+
+// foo
+# ^ source.terraform comment.line.terraform
+
+/////
+// Matches at random place in line + punctuation for `#`.
+/////
+
+        # bar
+#     ^ -comment -punctuation
+#       ^ punctuation.definition.comment.terraform
+#       ^^^^^ comment.line.terraform
+
+/////
+// Matches at random place in line + punctuation for `//`.
+/////
+
+    // baz # blah
+# ^ -comment -punctuation
+#   ^^ punctuation.definition.comment.terraform
+#   ^^^^^^^^^^^^^ comment.line.terraform
+
+/////////////////////////////////////////////////////////////////////
+// BLOCK COMMENTS
+/////////////////////////////////////////////////////////////////////
+
+/////
+// Matches for a single line.
+/////
+
+    /* foo */
+# ^ -comment -punctuation
+#   ^^ punctuation.definition.comment.terraform
+#   ^^^^^^^^ comment.block.terraform
+#          ^^ punctuation.definition.comment.terraform
+
+/////
+// Matches over multiple lines.
+/////
+
+    /*
+# ^ -comment -punctuation
+#   ^^ punctuation.definition.comment.terraform
+#   ^^^^ comment.block.terraform
+
+  foo
+# ^^^^ comment.block.terraform
+
+    */
+# ^^^^ comment.block.terraform
+#   ^^ punctuation.definition.comment.terraform
+
+/////
+// Matches inline comments after block ends.
+/////
+
+     /* comment */    // inline
+# ^ -comment -punctuation
+#    ^^ punctuation.definition.comment.terraform
+#    ^^^^^^^^^^^^ comment.block.terraform
+#               ^^ punctuation.definition.comment.terraform
+#                 ^^^ -comment -punctuation
+#                     ^^ punctuation.definition.comment.terraform
+#                     ^^^^^^^^ comment.line.terraform
+
+/////////////////////////////////////////////////////////////////////
+// LANGUAGE CONSTANTS
+/////////////////////////////////////////////////////////////////////
+
+/////
+// Matches `true`.
+/////
+
+    true
+# ^ -constant
+#   ^^^^ constant.language.terraform
+#         ^ -constant
+
+/////
+// Matches `false`.
+/////
+
+    false
+# ^ -constant
+#   ^^^^^ constant.language.terraform
+#         ^ -constant
+
+
+/////
+// Matches `null`.
+/////
+
+    null
+# ^ -constant
+#   ^^^^ constant.language.terraform
+#         ^ -constant
